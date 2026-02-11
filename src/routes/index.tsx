4cafed95e6ba6a8b3df8b630e984161ccf000397
DouglasFeliphe <douglasfeliphe@hotmail.com>
2026-02-11 15:53:49 -0300

    feat: implement conditional routing based on static authentication status

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../features/Dashboard';
 import { LoginForm } from '../features/SignIn/admin-login-form';
-// import Dashboard from '../features/Dashboard';
+import HomeLayout from '../layout/HomeLayout';
 
 export const AppRoutes = () => {
+  const isAuthenticated = true; // Replace with actual authentication logic
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-        </Routes>
+        {!isAuthenticated ? (
+          <Routes>
+            <Route path="/" index element={<LoginForm />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeLayout />}>
+              <Route path="/dashboard" index element={<Dashboard />} />
+              <Route path="*" index element={<h1>Page Not Found</h1>} />
+            </Route>
+          </Routes>
+        )}
       </BrowserRouter>
     </>
   );
